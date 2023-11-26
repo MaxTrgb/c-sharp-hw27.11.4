43302c154bc86568c089814365861ede5fb9cb28
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-11-26 16:18:32 +0200

    Add files via upload

---
+++ C#hw27.11.3/C#hw27.11.3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MySpace
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "John";
+            person.Age = 25;
+
+            Console.WriteLine($"Name: {person.Name}");
+            Console.WriteLine($"Age: {person.Age}");
+
+            person.Age = 30;
+
+            Console.WriteLine("\nUpdated Data:");
+            Console.WriteLine($"Name: {person.Name}");
+            Console.WriteLine($"Age: {person.Age}");
+        }
+    }
+}
